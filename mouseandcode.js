06f7fa67fd8cf1d24f932ecacfbeb62e73f8ab2e
macosta2015 <macosta2015@my.fit.edu>
2024-03-31 23:26:40 -0400

    Update mouseandcode.js

--- mouseandcode.js
+++ mouseandcode.js
@@ -36,55 +36,10 @@ const { performRightClickOptionByTitle } = require('./components/performRightCli
 
 
 
-        // const desiredOption = 'Copy sketch'; //TYPE WHICH EDIT OPTION YOU WANT TO CHOSE
-        // console.log('Desired rename option:', desiredOption);
-        // await new Promise(resolve => setTimeout(resolve, 5000));
-
-        // console.log('Searching for index of desired option...');
-        // await new Promise(resolve => setTimeout(resolve, 5000));
-        // const desiredOptionIndex = editOptions3.indexOf(desiredOption);
-        // console.log('Index of desired  option:', desiredOptionIndex);
-        // await new Promise(resolve => setTimeout(resolve, 5000));
-
-        // if (desiredOptionIndex !== -1) {
-        //     console.log('Desired rename option found.');
-        //     await new Promise(resolve => setTimeout(resolve, 5000));
-
-        //     console.log('Evaluating option element... ', desiredOption);
-        //     const renameOptionElement = await newPage.evaluateHandle((index) => {
-        //         const menuItems = document.querySelectorAll('.context-menu-item-span');
-        //         return menuItems[index];
-        //     }, desiredOptionIndex);
-        //     await new Promise(resolve => setTimeout(resolve, 5000));
-
-        //     if (renameOptionElement) {
-        //         console.log('Option element found.', desiredOption);
-        //         await new Promise(resolve => setTimeout(resolve, 5000));
-        //         await renameOptionElement.click();
-        //         console.log(`Clicked on ${desiredOption} option element.`);
-        //         await new Promise(resolve => setTimeout(resolve, 5000));
-        //     } else {
-        //         console.error(`${desiredOption} option element not found.`);
-        //         await new Promise(resolve => setTimeout(resolve, 5000));
-        //     }
-        // } else {
-        //     console.error(`${desiredOption} option not found.`);
-        //     await new Promise(resolve => setTimeout(resolve, 5000));
-        // }
-        // console.log('Waiting 10 seconds.');
-        // await new Promise(resolve => setTimeout(resolve, 10000)); // Wait for 10 seconds
-
-
-
-
-
-
-
         //SELECT SKETCH RIGHT CLICK OPTIONS TO UNCLICK
-        // console.log('RIGHT CLICK OPTIONS:');
-
-
         console.log('Waiting 10 seconds.');
+        console.log('SELECTING ITEM 5 ON THE LIST.');
+
         await new Promise(resolve => setTimeout(resolve, 10000)); // Wait for 10 seconds
 
 
@@ -98,9 +53,11 @@ const { performRightClickOptionByTitle } = require('./components/performRightCli
             }
         });
 
-        console.log('Waiting 10 seconds. AFTER ALL THE MESS');
-        await new Promise(resolve => setTimeout(resolve, 10000)); // Wait for 10 seconds
 
+
+        //ALL THE FOLLOWING CODE NEEDS TO GO TOGETHER
+        console.log('Waiting 5 seconds. AFTER ALL THE MESS');
+        await new Promise(resolve => setTimeout(resolve, 5000)); // Wait for 10 seconds
         const desiredIndex1 = 6; // Or any other desired index
         await clickButtonByIndex(newPage, desiredIndex1);
         //Right click option
